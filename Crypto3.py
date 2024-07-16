182561621d3151fcc47217beeae69cdaea9400a0
FIamberg <168779345+FIamberg@users.noreply.github.com>
2024-07-16 11:56:58 +0400

    Update Crypto3.py

--- Crypto3.py
+++ Crypto3.py
@@ -5,6 +5,7 @@ import datetime
 
 st.set_page_config(layout="wide")
 
+@st.cache_resource
 def connect_to_database():
     conn = mysql.connector.connect(
         host="185.120.57.125",
@@ -14,7 +15,6 @@ def connect_to_database():
     )
     return conn
 
-@st.cache_data
 @st.cache_resource
 def fetch_data(date_from=None, date_to=None):
     conn = connect_to_database()
@@ -26,8 +26,8 @@ def fetch_data(date_from=None, date_to=None):
         etherdrop_parser.datetime, 
         etherdrop_parser.dollar_value,
         CASE 
-            WHEN wallet_list.wallet_address = etherdrop_parser.pool_address_from THEN 'продажа'
-            WHEN wallet_list.wallet_address = etherdrop_parser.receiver_address_link THEN 'покупка'
+            WHEN wallet_list.wallet_address = etherdrop_parser.pool_address_from THEN 'кошелек продажи'
+            WHEN wallet_list.wallet_address = etherdrop_parser.receiver_address_link THEN 'кошелек покупки'
             ELSE 'пустой кошелек'
         END AS wallet_type
     FROM wallet_list
@@ -38,12 +38,16 @@ def fetch_data(date_from=None, date_to=None):
     """
 
     if date_from and date_to:
-        query += f" AND etherdrop_parser.datetime BETWEEN '{date_from}' AND '{date_to}'"
-
-    df = pd.read_sql(query, conn)
+        query += " AND etherdrop_parser.datetime BETWEEN %s AND %s"
+        df = pd.read_sql(query, conn, params=[date_from, date_to])
+    else:
+        df = pd.read_sql(query, conn)
+    
     conn.close()
-
     return df
+
+def make_wallet_address_link(wallet_address):
+    return f"https://www.alphatrace.xyz/wallet/{wallet_address}"
 
 def main():
     st.title('Wallets')
@@ -68,17 +72,17 @@ def main():
         unique_currency_names = df['currency_name'].unique().tolist()
         unique_wallet_types = df['wallet_type'].unique().tolist()
 
-        selected_currency = st.sidebar.selectbox("currency_name:", [""] + unique_currency_names)
+        # Сортировка списка валют по алфавиту
+        sorted_currency_names = sorted(unique_currency_names)
+        
+        selected_currency = st.sidebar.selectbox("currency_name:", [""] + sorted_currency_names)
         selected_username = st.sidebar.selectbox("wallet_address:", [""] + unique_usernames)
-        #selected_wallet_type = st.sidebar.selectbox("wallet_type:", [""] + unique_wallet_types)
-
+        
         # Применение фильтров
         if selected_username:
             df = df[df['wallet_address'] == selected_username]
         if selected_currency:
             df = df[df['currency_name'] == selected_currency]
-        #if selected_wallet_type:
-        #    df = df[df['wallet_type'] == selected_wallet_type]
 
         # Создание таблиц
         # 1. Детальная информация
@@ -87,37 +91,58 @@ def main():
 
         # 2. Сводная информация по валютам
         currency_summary = df.groupby('currency_name').agg({
-            'wallet_address': lambda x: x[df['wallet_type'] == 'покупка'].nunique(),
-            'dollar_value': lambda x: (x * (df['wallet_type'] == 'покупка')).sum()
-        }).rename(columns={'wallet_address': 'Кошельки покупки', 'dollar_value': 'Объем покупки $'})
+            'wallet_address': lambda x: x[df['wallet_type'] == 'кошелек покупки'].nunique(),
+            'dollar_value': lambda x: (x * (df['wallet_type'] == 'кошелек покупки')).sum()
+        }).rename(columns={'wallet_address': 'buy_wallets_count', 'dollar_value': 'buy_volume'})
         
-        currency_summary['Кошельки продажи'] = df[df['wallet_type'] == 'продажа'].groupby('currency_name')['wallet_address'].nunique()
-        currency_summary['Объем продажи'] = df[df['wallet_type'] == 'продажа'].groupby('currency_name')['dollar_value'].sum()
+        currency_summary['sell_wallets_count'] = df[df['wallet_type'] == 'кошелек продажи'].groupby('currency_name')['wallet_address'].nunique()
+        currency_summary['sell_volume'] = df[df['wallet_type'] == 'кошелек продажи'].groupby('currency_name')['dollar_value'].sum()
         currency_summary = currency_summary.reset_index()
 
         # 3. Информация по кошелькам
         wallet_info = df.groupby('wallet_address').agg({
             'dollar_value': lambda x: [
-                (x * (df['wallet_type'] == 'покупка')).sum(),
-                (x * (df['wallet_type'] == 'продажа')).sum()
+                (x * (df['wallet_type'] == 'кошелек покупки')).sum(),
+                (x * (df['wallet_type'] == 'кошелек продажи')).sum()
             ]
         }).reset_index()
         wallet_info[['buy_volume', 'sell_volume']] = pd.DataFrame(wallet_info['dollar_value'].tolist(), index=wallet_info.index)
         wallet_info = wallet_info.drop('dollar_value', axis=1)
+
+        # Добавляем столбец со ссылкой
+        wallet_info['wallet_link'] = wallet_info['wallet_address'].apply(make_wallet_address_link)
+        #wallet_info = wallet_info[['wallet_address', 'wallet_link', 'buy_volume', 'sell_volume']]  # Перестановка столбцов
+        wallet_info = wallet_info.rename(columns={'wallet_link': 'Wallet Link'})  # Переименование столбца
 
         # Отображение таблиц
         col1, col2 = st.columns([1, 2])
 
         with col1:
             st.subheader("Сводная информация по валютам")
-            st.dataframe(currency_summary, use_container_width=True, height=500, hide_index=True)
+            st.dataframe(currency_summary, use_container_width=True, height=500)
 
             st.subheader("Информация по кошелькам")
-            st.dataframe(wallet_info, use_container_width=True, height=400, hide_index=True)
+            # Используем st.dataframe для отображения таблицы wallet_info
+            st.dataframe(
+                wallet_info[['wallet_address', 'buy_volume', 'sell_volume', 'Wallet Link']],
+                column_config={
+                    "Wallet Link": st.column_config.LinkColumn(
+                        label="Wallet Address",
+                        display_text = "Link",
+                        #url_column="Wallet Link",
+                        help="Click to open wallet"
+                    ),
+                    "buy_volume": "Buy Volume",
+                    "sell_volume": "Sell Volume"
+                },
+                hide_index=True,
+                use_container_width=True,
+                height=400
+            )
 
         with col2:
             st.subheader("Детальная информация")
-            st.dataframe(detailed_info, use_container_width=True, height=982, hide_index=True)
+            st.dataframe(detailed_info, use_container_width=True, height=982)
 
     else:
         st.error("Пожалуйста, выберите диапазон дат.")
